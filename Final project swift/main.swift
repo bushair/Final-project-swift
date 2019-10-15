baec96bb6b37b80eac22642140dea340d476c99a
bushair <bushairkm007@gmail.com>
2019-10-15 18:21:47 -0400

    Initial Commit

---
+++ Final project swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Final project swift
+//
+//  Created by MacStudent on 2019-10-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
